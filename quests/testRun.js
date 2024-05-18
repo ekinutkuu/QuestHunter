251f356abccc7ff90ac27e281e0f0f07fdbb31b9
Ekin Utku <ekinutkugulmez@gmail.com>
2024-05-18 20:52:16 +0300

    submit button disabled and cache bug fixed

--- quests/testRun.js
+++ quests/testRun.js
@@ -1,14 +1,27 @@
 function testRun(phpFile) {
     var userCode = document.querySelector('textarea[name="user_code"]').value;
+    var submitButton = document.getElementById('submit-button');
+    var resultMessage = document.getElementById('output');
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", phpFile, true);
+    xhr.open("POST", phpFile + "?t=" + new Date().getTime(), true);
+    //kodun normal hali -> xhr.open("POST", phpFile, true);
+    //cache'e alınmasını önlemek için anlık olarak tarihi alıp request url'ye eklemektedir
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response = xhr.responseText;
-            document.getElementById("output").innerHTML = response;
-            console.log(response);
+
+            if (response.trim() === "passed") {
+                submitButton.classList.remove('disabled');
+                submitButton.setAttribute("onclick", "submitCode()");
+                resultMessage.innerHTML = "Correct Answer!";
+            } else {
+                submitButton.classList.add('disabled');
+                submitButton.removeAttribute("onclick");
+                resultMessage.innerHTML = "Wrong Answer!";
+            }
+
         }
     };
     xhr.send("user_code=" + encodeURIComponent(userCode));
